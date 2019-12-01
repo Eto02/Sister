bb1c74c87dac6dd99dbb7d63765e2de4eb8ad444
Tahta FM <mmikoto89@gmail.com>
2019-12-01 13:41:39 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,14 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
 
     Route::group(['prefix' => 'mahasiswa'], function() {
-        ;
+
         Route::get('/getMahasiswa', 'MahasiswaController@getMahasiswa')->name('getMahasiswa');
         Route::get('/getBiodataMahasiswa', 'MahasiswaController@getBiodataMahasiswa')->name('getBiodataMahasiswa');
         Route::get('/insertBiodataMahasiswa', 'MahasiswaController@insertBiodataMahasiswa')->name('insertBiodataMahasiswa');
         Route::get('/DeleteBiodataMahasiswa/{id}', 'MahasiswaController@DeleteBiodataMahasiswa')->name('DeleteBiodataMahasiswa');
     });
     Route::group(['prefix' => 'umum'], function() {
+        Route::get('/GetProfilPT', 'GeneralController@GetProfilPT')->name('GetProfilPT');
         Route::get('/GetToken', 'GeneralController@GetToken')->name('GetToken');
         Route::get('/GetJenjangPendidikan', 'GeneralController@GetJenjangPendidikan')->name('GetJenjangPendidikan');
         Route::get('/GetPenghasilan/{id}', 'GeneralController@GetPenghasilan')->name('GetPenghasilan');
@@ -34,9 +35,23 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('/GetAlatTransportasi', 'GeneralController@GetAlatTransportasi')->name('GetAlatTransportasi');
         Route::get('/GetNegara', 'GeneralController@GetNegara')->name('GetNegara');
         Route::get('/GetAgama/{id}', 'GeneralController@GetAgama')->name('GetAgama');
+        Route::get('/GetJenisPendaftaran', 'GeneralController@GetJenisPendaftaran')->name('GetJenisPendaftaran');
+        Route::get('/GetJalurMasuk', 'GeneralController@GetJalurMasuk')->name('GetJalurMasuk');
+        Route::get('/GetSemester', 'GeneralController@GetSemester')->name('GetSemester');
+        Route::get('/GetProdi', 'GeneralController@GetProdi')->name('GetProdi');
+        Route::get('/GetAllPT', 'GeneralController@GetAllPT')->name('GetAllPT');
+        Route::get('/GetAllProdi', 'GeneralController@GetAllProdi')->name('GetAllProdi');
+
 
     });
 
 
     Route::get('/home', 'HomeController@index')->name('home');
+    Route::group(['prefix' => 'riwayat'], function() {
+
+        Route::get('/GetListRiwayatPendidikanMahasiswa', 'RiwayatPendidikanController@GetListRiwayatPendidikanMahasiswa')->name('GetListRiwayatPendidikanMahasiswa');
+        Route::get('/getBiodataMahasiswa', 'RiwayatPendidikanController@getBiodataMahasiswa')->name('getBiodataMahasiswa');
+        Route::get('/insertBiodataMahasiswa', 'RiwayatPendidikanController@insertBiodataMahasiswa')->name('insertBiodataMahasiswa');
+        Route::get('/DeleteBiodataMahasiswa/{id}', 'RiwayatPendidikanController@DeleteBiodataMahasiswa')->name('DeleteBiodataMahasiswa');
+    });
 });
